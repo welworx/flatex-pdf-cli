3aa9849f63e9038982525a8ee94d037f5eefb764
Werner Liemberger <dev@liemberger.cc>
2026-06-24 17:03:47 +0200

    chore: initialize Go project with dependencies and linting config

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	// pdfcpu will be used by internal/extractor in future tasks
+	_ "github.com/pdfcpu/pdfcpu/pkg/pdfcpu"
+)
+
+func main() {
+	// TODO: Implement CLI entry point
+}
